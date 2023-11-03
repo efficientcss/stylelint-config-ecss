43b9b74475279e67977729bef655010a67cd706e
Marc-André Charpentier <emma.charp@me.com>
2023-11-03 07:51:15 -0400

    Better file management.

--- plugins/stylelint-selector-starts-with-filename.js
+++ plugins/stylelint-selector-starts-with-filename.js
@@ -2,7 +2,7 @@ const stylelint = require("stylelint");
 const report = stylelint.utils.report;
 const ruleMessages = stylelint.utils.ruleMessages;
 const validateOptions = stylelint.utils.validateOptions;
-
+const { isString } = require("../lib/validateTypes");
 const path = require("path");
 
 const ruleName = 'plugin/selector-starts-with-filename';
@@ -10,14 +10,31 @@ const messages = ruleMessages(ruleName, {
 	rejected: (selector, filename) => `All selectors must begin with filename. ${selector} vs. ${filename}`
 });
 
-const rule = () => {
+const rule = (primary, secondaryOptions) => {
 	return (root, result) => {
-		const validOptions = validateOptions(result, ruleName);
-		const filename = path.parse(root.source.input.file).name;
+		const validOptions = validateOptions(
+			result, 
+			ruleName,
+			{ actual: primary },
+			{
+				actual: secondaryOptions,
+				possible: {
+					ignoreFiles: [isString],
+				},
+				optional: true,
+			},
+		);
+		const inputFile = root.source.input.file;
+		const filename = path.parse(inputFile).name;
 		const selectorPattern = '^((\\* \\+ |\\* ~ )?(\\.|\\[.*=)?)?(\")?'+filename+'(?:-[a-zA-Z]+)?(\")?(\\])?.*';
 		const selectorRegExp = new RegExp(selectorPattern, 'g');
 
 		if (!validOptions) {
+			return;
+		}
+
+		// skip filenames starting with digit or "x-" pattern
+		if (filename.match(/^\d/) || filename.startsWith("x-")) {
 			return;
 		}
 
