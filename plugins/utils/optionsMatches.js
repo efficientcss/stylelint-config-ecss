53cdd9c3238a232a2d09435525b7f2f5a81d89f2
Marc-André Charpentier <emma.charp@me.com>
2024-06-15 08:02:01 -0400

    Rogue console.log

--- plugins/utils/optionsMatches.js
+++ plugins/utils/optionsMatches.js
@@ -1,7 +1,6 @@
 import matchesStringOrRegExp from './matchesStringOrRegExp.js';
 
 export default function optionsMatches(options, propertyName, input) {
-	console.log(options[propertyName]);
 	return Boolean(
 		options &&
 			options[propertyName] &&
