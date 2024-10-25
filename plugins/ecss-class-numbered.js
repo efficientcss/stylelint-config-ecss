8bb6094020e14add9dbbc28c1a7915688180dc58
Marc-André Charpentier <emma.charp@me.com>
2024-10-25 10:04:39 -0400

    Indentation.

--- plugins/ecss-class-numbered.js
+++ plugins/ecss-class-numbered.js
@@ -1,34 +1,34 @@
 import stylelint from 'stylelint';
 
 const {
-  createPlugin,
-  utils: { report, ruleMessages }
+	createPlugin,
+	utils: { report, ruleMessages }
 } = stylelint;
 
 const ruleName = 'ecss/class-numbered';
 const messages = ruleMessages(ruleName, {
-  expected: 'Avoid numbers in class names.',
+	expected: 'Avoid numbers in class names.',
 });
 
 const meta = {
-  url: 'https://example.com/rules/class-numbered'
+	url: 'https://example.com/rules/class-numbered'
 };
 
 const ruleFunction = (primaryOption, secondaryOption, context) => {
-  return (postcssRoot, postcssResult) => {
-    const numberedClassRegex = /\.(?!(h[1-6]|grid-[0-9]+|col-[0-9]+)$)[a-zA-Z-]*[0-9]+/;
+	return (postcssRoot, postcssResult) => {
+		const numberedClassRegex = /\.(?!(h[1-6]|grid-[0-9]+|col-[0-9]+)$)[a-zA-Z-]*[0-9]+/;
 
-    postcssRoot.walkRules((rule) => {
-      if (numberedClassRegex.test(rule.selector)) {
-        report({
-          message: messages.expected,
-          node: rule,
-          result: postcssResult,
-          ruleName,
-        });
-      }
-    });
-  };
+		postcssRoot.walkRules((rule) => {
+			if (numberedClassRegex.test(rule.selector)) {
+				report({
+					message: messages.expected,
+					node: rule,
+					result: postcssResult,
+					ruleName,
+				});
+			}
+		});
+	};
 };
 
 ruleFunction.ruleName = ruleName;
