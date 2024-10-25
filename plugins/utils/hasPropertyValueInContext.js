f9fea0ca41dffef2dfad77972d3211c19ca3d901
Marc-André Charpentier <emma.charp@me.com>
2024-10-25 10:06:58 -0400

    Accept property patterns as param.

--- plugins/utils/hasPropertyValueInContext.js
+++ plugins/utils/hasPropertyValueInContext.js
@@ -1,11 +1,11 @@
 /**
 * @param {object} rule
-* @param {string} property
+* @param {string} propertyPattern
 * @param {RegExp|string} valuePattern
 * @param {string} context
 * @returns {boolean}
 */
-export default function hasPropertyValueInContext(rule, property, valuePattern, context) {
+export default function hasPropertyValueInContext(rule, propertyPattern, valuePattern, context) {
 
 	const isDescendant = /&\s*(?:>|\s+\.)/.test(rule.selector);
 	const isCombined = /&(:[\w-]+|::[\w-]+|\[.*?\]|\.[\w-]+|#\w+)/u.test(rule.selector);
@@ -13,13 +13,18 @@ export default function hasPropertyValueInContext(rule, property, valuePattern, 
 	let currentRule = context == 'parent' && !isCombined ? rule.parent : rule;
 	
 	while (currentRule && currentRule.type === 'rule') {
-		if (currentRule.some((decl) => 
-			decl.prop === property && (
-				valuePattern instanceof RegExp 
-				? valuePattern.test(decl.value)
-				: decl.value === valuePattern
-			)
-		)) {
+
+		const hasPropertyValue = currentRule.some((decl) => (
+			propertyPattern instanceof RegExp
+			? propertyPattern.test(decl.prop)
+			: decl.prop === propertyPattern
+		) && (
+			valuePattern instanceof RegExp 
+			? valuePattern.test(decl.value)
+			: decl.value === valuePattern
+		));
+
+		if (hasPropertyValue) {
 			return true;
 		}
 		if(context == 'self' && isCombined || context == 'parent' && isDescendant) {
