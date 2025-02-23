678a995e60334bbdcd56ba5a8baa8b4c68e98fdb
Marc-André Charpentier <emma.charp@me.com>
2025-02-23 08:33:15 -0500

    Prevent flagging on graphical selectors.

--- plugins/ecss-width-100p.js
+++ plugins/ecss-width-100p.js
@@ -24,9 +24,10 @@ const preprocessCSS = async (css) => {
 
 const ruleFunction = (primaryOption, secondaryOption, context) => async (postcssRoot, postcssResult) => {
 	const processedRoot = await preprocessCSS(postcssRoot.toString());
+	const notGraphicalSelectorsRegex = /^(?!.*(?:image|img|video|hr|picture|photo|icon|i$|shape|before$|after$|figure|hr$|svg|line|logo|frame|button|input|select$|textarea)).*$/;
 	processedRoot.walkRules((rule) => {
 		rule.walkDecls('width', (decl) => {
-			if (/^100%$/.test(decl.value)) {
+			if (/^100%$/.test(decl.value) && notGraphicalSelectorsRegex.test(rule.selector)) {
 				report({
 					message: messages.expected,
 					messageArgs: [rule.selector, decl],
