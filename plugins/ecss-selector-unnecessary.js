2c3d5634613d2cff6100885815c04b8840554015
Marc-André Charpentier <emma.charp@me.com>
2025-02-23 08:32:08 -0500

    Prevent flagging of element as first selector.

--- plugins/ecss-selector-unnecessary.js
+++ plugins/ecss-selector-unnecessary.js
@@ -18,7 +18,7 @@ const meta = {
 
 const ruleFunction = (primaryOption, secondaryOption, context) => {
 	return (postcssRoot, postcssResult) => {
-		const overlyStructuredChildrenRegex = /^(?!.*(?:>|~)).*\b(?<![.-])(?:section|div|footer|aside|article|ul|li|body)\b(?![.-]).*\b(?:p|ul|li|a|button|input|span|h[1-6])\b$/;
+		const overlyStructuredChildrenRegex = /^(?!.*(?:>|~))(?=.*\S\s+\b(?<![-.\w])(?:section|div|footer|aside|article|ul|li|body)\b(?![.-])(?=.*\b(?:p|ul|li|a|button|input|span|h[1-6])\b)).*$/;
 		const necessaryStructuredChildrenRegex = /ul ul|li li/;
 		postcssRoot.walkRules((rule) => {
 			if (overlyStructuredChildrenRegex.test(rule.selector) && !necessaryStructuredChildrenRegex.test(rule.selector)) {
